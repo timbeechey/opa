58c7fec9570d260eda28008635c625c69f0bab9d
timb <tim.beechey@protonmail.com>
2024-02-15 22:48:34 +0000

    Added data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Childhood growth data
+#' 
+#'  Distance from the the pituitary to the pteryo-maxillary fissure.
+#' 
+#' @format ## `pituitary`
+#' A data frame with 108 rows and 4 columns:
+#' \describe{
+#'  \item{distance}{distance in mm from the pituitary to the pteryo-maxillary fissure}
+#'  \item{age}{age in years}
+#'  \item{individual}{identifier for each individual}
+#'  \item{sex}{sex of each individual}
+#' }
+#' @source Potthoff, R. F., & Roy, S. N. (1964). A Generalized Multivariate Analysis of Variance Model Useful Especially for Growth Curve Problems. Biometrika, 51(3/4), 313–326. https://doi.org/10.2307/2334137
+"pituitary"
