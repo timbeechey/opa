6b45a392239ee61745774e68aac3556ce15cde80
timb <tim.beechey@protonmail.com>
2024-02-17 19:47:47 +0000

    Formatted data documentation

--- R/data.R
+++ R/data.R
@@ -1,14 +1,15 @@
 #' Childhood growth data
+#' 
+#' @description
 #'
-#' Distance from the the pituitary to the pteryo-maxillary fissure.
+#' A data frame with 108 rows and 4 columns containing data on the distance from the the pituitary to the pteryo-maxillary fissure in children.
 #'
-#' @format ## `pituitary`
-#' A data frame with 108 rows and 4 columns:
+#' @format
 #' \describe{
-#'  \item{distance}{distance in mm from the pituitary to the pteryo-maxillary fissure}
-#'  \item{age}{age in years}
-#'  \item{individual}{identifier for each individual}
-#'  \item{sex}{sex of each individual}
+#'  \item{distance}{Distance in mm from the pituitary to the pteryo-maxillary fissure.}
+#'  \item{age}{Age in years.}
+#'  \item{individual}{Unique identifier for each individual.}
+#'  \item{sex}{Biological sex of each individual.}
 #' }
 #' @source Potthoff, R. F., & Roy, S. N. (1964). A Generalized Multivariate Analysis of Variance Model Useful Especially for Growth Curve Problems. Biometrika, 51(3/4), 313–326. https://doi.org/10.2307/2334137
 "pituitary"
@@ -16,10 +17,10 @@
 
 #' Bee data
 #' 
-#' Time between visits to a mechanical flower by bees in two experimental conditions.
+#' @description
+#' A data frame with 20 rows and 14 columns containing times between visits to a mechanical flower by bees in two experimental conditions.
 #' 
-#' @format ## `bees`
-#' A data frame with 20 rows and 14 columns:
+#' @format
 #' \describe{
 #'  \item{bee}{Unique identifier for each individual bee.}
 #'  \item{condition}{Factor identifying the two experimental conditions. In the frustrated condition bees were temporarily restricted from returning to the hive after collecting nectar, in the free condition bees were able to return to the hive without delay.}
